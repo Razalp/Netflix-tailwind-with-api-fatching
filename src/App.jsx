6499980e77d0148221b4599ab1b701d6c199fc51
Razalp <razalp0012300@gmail.com>
2023-12-07 14:16:39 +0530

    create banner and i add photos

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Navbar from './componets/NavBar/Navbar'
+import Banner from './componets/Banner/Banner'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navbar/>
+
+    
+      <Banner/>
     </>
   )
 }

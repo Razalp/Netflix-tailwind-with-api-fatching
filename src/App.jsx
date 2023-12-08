ca04321aae42d0df0542f4ed1c53fe1e6c09ec37
Razalp <razalp0012300@gmail.com>
2023-12-08 13:21:03 +0530

    creating netfix same changes with no error

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './componets/NavBar'
+import Home from './Pages/Home'
 
-
+import {Route , Routes} from 'react-router-dom';
 function App() {
 
 
   return (
     <>
      <NavBar/>
+     <Routes>
+      <Route path='/' elmenet={<Home/>}></Route>
+     </Routes>
     </>
   )
 }
